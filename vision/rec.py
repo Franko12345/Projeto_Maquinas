98863145739a02b44d93276fba1849cc576ad4d2
Franco12345 <franco@delucca.pro.br>
2025-11-07 11:30:43 -0300

    Added 3d files and changed the vision algorithm to give the coordinates of the offset from the middle

--- vision/rec.py
+++ vision/rec.py
@@ -4,15 +4,32 @@ face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(2)
+
+if not video_capture.isOpened():
+    print("Camera nao disponivel")
+
+else:
+    width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
 
 def detect_bounding_box(vid):
+    global width, height
     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
-    for (x, y, w, h) in faces:
-        cv2.circle(vid, (int(x+w/2), int(y+h/2)), 5, (0, 255, 0), 4)
-        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-    return faces
+
+    if len(faces) == 0:
+        return 0
+
+    face = max(faces, key=lambda x: x[2]*x[3])
+    (x, y, w, h) = face
+
+    cv2.circle(vid, (int(x+w/2), int(y+h/2)), 5, (0, 255, 0), 4)
+    cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    cv2.putText(vid, f"X: {int(round((x+w/2)-round(width/2)))} Y:{int(round((y+h/2)-round(height/2)))}", (x, y), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255))
+
+    return (x+w/2, y+h/2)
 
 while True:
     result, video_frame = video_capture.read()
